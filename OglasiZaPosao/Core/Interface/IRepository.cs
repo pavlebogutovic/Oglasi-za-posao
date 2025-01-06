3569f6df33377e4ef53c7631417918e07f23551a
pavlebogutovic <121833565+pavlebogutovic@users.noreply.github.com>
2025-01-06 14:54:19 +0100

    folder

---
+++ OglasiZaPosao/Core/Interface/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interface
+{
+    public interface IRepository<T>
+    {
+        List<T> GetAll();
+        bool Add(T item);
+        bool Update(T item);
+        bool Delete(T item);
+    }
+}
